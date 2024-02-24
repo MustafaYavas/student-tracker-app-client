29aa8a6ba7976d4a67ee179906db0a8357e9ba94
MustafaYavas <mustafayavas40@gmail.com>
2024-02-24 17:10:52 +0300

    Add type annotation for add student function

--- src/pages/AddStudentPage.js
+++ src/pages/AddStudentPage.js
@@ -6,6 +6,16 @@ import * as actionTypes from '../redux/actionTypes';
 const AddStudentPage = () => {
   const dispatch = useDispatch();
 
+  /**
+   *
+   * @typedef {Object} newStudent
+   * @property {string} firstName - first name
+   * @property {string} lastName - last name
+   * @property {string} email - email
+   * @property {string} departments - departments
+   */
+
+  /** @param {newStudent} - params */
   const addStudent = (newStudent) => {
     dispatch({ type: actionTypes.STUDENT_ADD_REQUEST, payload: newStudent });
   };
