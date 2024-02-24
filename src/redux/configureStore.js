b3194858b2640bc39ef7ef1798fd0790093a966a
MustafaYavas <mustafayavas40@gmail.com>
2024-02-24 16:14:06 +0300

    Remove redux package

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,17 +1,14 @@
-import { combineReducers } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { configureStore } from '@reduxjs/toolkit';
 import StudentsReducer from './reducers/StudentsReducer';
 import Saga from './saga/Saga.js';
 
-const rootReducer = combineReducers({
-  students: StudentsReducer,
-});
-
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    students: StudentsReducer,
+  },
   middleware: () => [sagaMiddleware],
 });
 
